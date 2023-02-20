6f5751ea3a93bcb8d4de34d32032774b46aac383
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-02-19 23:43:54 -0500

    simulator

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import simulator
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+map1 = (1, [(1.0, 2.0, 3.0, 4.0), (3.0, 4.0, 5.0, 6.0)])
+map2 = (2, [(5.0, 6.0, 7.0, 8.0), (7.0, 8.0, 9.0, 10.0)])
+map3 = (3, [(2.0, 3.0, 4.0, 5.0), (6.0, 7.0, 8.0, 9.0)])
+maps = [map1, map2, map3]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+grouped_data_points = simulator.data_fusion(maps)
+print(grouped_data_points)
